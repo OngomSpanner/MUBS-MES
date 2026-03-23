dea3a71e7a4acf20e793ad0239c81363daed6e28
Ongom Spanner Ivan <ongomiv2@gmail.com>
2026-03-23 07:33:35 +0300

    updated

--- tmp_describe.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=mubs_super_admin','root','');
-    $stmt = $pdo->query('DESCRIBE strategic_activities');
-    print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
